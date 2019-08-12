b12f85b070f74dc9501ef5e8a58195ee6a0153f1
Ivan Zheng <yvonne_7_7@hotmail.com>
2019-08-12 09:28:37 +0800

    Add ConfigurationExtension

---
+++ samples/Quickstarts/9_Combined_AspId_and_EFStorage/IdentityServer4Client/ConfigurationExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IdentityServer4.AccessTokenValidation;
+using IdentityServer4Client.Authorizations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Refit;
+
+namespace IdentityServer4Client
+{
+    public static class ConfigurationExtension
+    {
+        public static IServiceCollection AddIdentityServerClient(this IServiceCollection services)
+        {
+            services.AddRefitClient<IPermissionStore>()
+                    .ConfigureHttpClient((provider, httpClient) =>
+                    {
+                        var options = provider.GetService<IOptions<IdentityServerAuthenticationOptions>>();
+                        httpClient.BaseAddress = new Uri(options.Value.Authority);
+                    });
+
+            return services;
+        }
+    }
+}
